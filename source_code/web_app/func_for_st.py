d2fb1bdbd3e16d9610e125eaf7cf973f4e4d31df
Lejun Chen <chenlejun@gmail.com>
2022-04-21 01:31:15 +0200

    requirement.txt

--- source_code/web_app/func_for_st.py
+++ source_code/web_app/func_for_st.py
@@ -955,7 +955,7 @@ def preprocess_data(input_df_pressure,input_df_rate,denoise):
         pressure_df=processed_data_denoised.pressure_df
         rate_df=processed_data_denoised.rate_df
         # pressure_df=pressure_df[0:12000]
-        pressure_df=pressure_df[0:3000]
+        # pressure_df=pressure_df[0:3000]
        
         return pressure_df,rate_df
 
